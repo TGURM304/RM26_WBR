c0221bc8c0df939577543149932dd44b23766935
KLnormal <yuyongyikaf530000@163.com>
2026-01-17 16:14:59 +0800

    finish motor pkg

---
+++ APP/chassis/components/app_motor_pkg.cpp
@@ -0,0 +1,82 @@
+//
+// Created by 15082 on 2026/1/16.
+//
+
+#include "app_motor_pkg.h"
+
+void Motor_Pkg::Joint::pkg_init() {
+    init();
+    enable();
+}
+
+void Motor_Pkg::Joint::set_tor(float tor) {
+    control(0,0,0,0,tor*dir_);
+}
+
+void Motor_Pkg::Joint::rest() {
+    control(0,0,0,0,0);
+    reset();
+}
+
+Motor_Pkg::motor_status_pkg Motor_Pkg::Joint::get_status() {
+    status_.old_pos = status_.pos;
+    status_.old_speed = status_.speed;
+    status_.pos = status.pos*dir_;
+    status_.speed = status.vel*dir_;
+    return status_;
+}
+
+
+
+void Motor_Pkg::Dynamic::pkg_init() {
+    init();
+    enable();
+}
+/*
+ * 铁损阻力：72rpm/Nm
+ * 减速比原装：3591/187
+ * 减速比：268/17
+ * 扭矩常数：0.3Nm/A
+ * 机械转角：0~8192
+ * 转子转速单位：RPM
+ * 电流： -20A~20A  -16384~16384
+ */
+#define REDUCTION_ORG (3591.0f/187.0f)
+#define REDUCTION_NOW (268.0f/17.0f)
+#define TORQUE_CONST (0.3f)
+#define PI_F32 (3.1415926f)
+void Motor_Pkg::Dynamic::set_tor(float tor) {
+    float temp_tor = tor*dir_;
+    float current = temp_tor/(REDUCTION_NOW/REDUCTION_ORG*TORQUE_CONST);
+    if(current > 20.0f) current = 20.0f;
+    if(current < -20.0f) current = -20.0f;
+    update(current*16384);
+}
+
+void Motor_Pkg::Dynamic::rest() {
+    update(0);
+}
+
+Motor_Pkg::motor_status_pkg Motor_Pkg::Dynamic::get_status() {
+    old_encode_ = encode_;
+    status_.old_pos = status_.pos;
+    status_.old_speed = status_.speed;
+
+    encode_ = status.angle;
+    float delta_encode = (encode_ - old_encode_)*dir_;
+    if(abs(encode_ - old_encode_) > 4096.0f) {
+        delta_encode >0 ? delta_encode -= 8192.0f : delta_encode += 8192.0f;
+    }
+    status_.pos += delta_encode/(8192.0f)*2.0f*PI_F32/(REDUCTION_NOW)*wheel_R_;
+    status_.speed = status.speed*dir_*2.0f*PI_F32/60.0f/(REDUCTION_NOW)*wheel_R_;
+    return status_;
+}
+
+void Motor_Pkg::Dynamic::status_clear() {
+    status_.old_pos = 0.0f;
+    status_.old_speed = 0.0f;
+    status_.pos = 0.0f;
+    status_.speed = 0.0f;
+    encode_ = status.angle;
+    old_encode_ = encode_;
+}
---
+++ APP/chassis/components/app_motor_pkg.h
@@ -0,0 +1,61 @@
+//
+// Created by 15082 on 2026/1/16.
+//
+
+#ifndef APP_MOTOR_PKG_H
+#define APP_MOTOR_PKG_H
+#include "dev_motor_dji.h"
+#include "dev_motor_dm.h"
+namespace Motor_Pkg {
+    typedef struct {
+        float old_pos, old_speed;
+        float pos;
+        float speed;
+    }motor_status_pkg;
+    typedef enum {
+        E_forward,
+        E_backward
+    }E_dir;
+    class Joint: Motor::DMMotor {
+    public:
+        explicit Joint(const char *name, const Model &model, const Param &param,E_dir dir)
+            :DMMotor(name,model,param){
+            if(dir == E_forward)
+                dir_ = 1.0f;
+            else if(dir == E_backward)
+                dir_ = -1.0f;
+        }
+        void pkg_init();
+        void set_tor(float tor);
+        void rest();
+        motor_status_pkg get_status();
+    private:
+        motor_status_pkg status_ = {0};
+        float dir_;
+    };
+    class Dynamic: Motor::DJIMotor {
+        explicit Dynamic(const char *name, const Model &model, const Param &param,
+            E_dir dir, float reduction, float wheel_R)
+        :DJIMotor(name,model,param), reduction_(reduction), wheel_R_(wheel_R){
+            if(dir == E_forward)
+                dir_ = 1.0f;
+            else if(dir == E_backward)
+                dir_ = -1.0f;
+        }
+        void pkg_init();
+        void set_tor(float tor);
+        void rest();
+        void status_clear();
+        motor_status_pkg get_status();
+    private:
+        motor_status_pkg status_ = {0};
+        float dir_;
+        float reduction_;
+        float wheel_R_;
+        float encode_ = 0.0f, old_encode_ = 0.0f;
+    };
+}
+
+
+
+#endif //APP_MOTOR_PKG_H
