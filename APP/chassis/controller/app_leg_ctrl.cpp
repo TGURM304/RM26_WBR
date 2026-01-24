ca4c15ba5750e3ebd3df7c504ca7c4f73d5c0648
KLnormal <yuyongyikaf530000@163.com>
2026-01-25 02:26:04 +0800

    添加了腿部控制器的框架，大抵是没有问题的

---
+++ APP/chassis/controller/app_leg_ctrl.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 15082 on 2026/1/24.
+//
+
+#include "app_leg_ctrl.h"
+
+void Controller::app_leg_ctrl::left_len_update(Relay::leg_status left_leg, float target_len) {
+    float32_t target_speed = left_length_pid_.update(left_leg.L0,target_len);
+    output_.force_left = left_speed_pid_.update(left_leg.dot_L0,target_speed);
+}
+void Controller::app_leg_ctrl::right_len_update(Relay::leg_status right_leg, float target_len) {
+    float32_t target_speed = right_length_pid_.update(right_leg.L0,target_len);
+    output_.force_right = right_speed_pid_.update(right_leg.dot_L0,target_speed);
+}
+void Controller::app_leg_ctrl::left_deg_update(Relay::leg_status left_leg, float target_deg) {
+    float32_t target_omega = left_deg_pid_.update(left_leg.theta,target_deg);
+    output_.tor_left = left_omega_pid_.update(left_leg.dot_theta,target_omega);
+}
+void Controller::app_leg_ctrl::right_deg_update(Relay::leg_status right_leg, float target_deg) {
+    float32_t target_omega = right_deg_pid_.update(right_leg.theta,target_deg);
+    output_.tor_right = right_omega_pid_.update(right_leg.dot_theta,target_omega);
+}
+void Controller::app_leg_ctrl::leg_clear() {
+    left_speed_pid_.clear();
+    right_speed_pid_.clear();
+    left_deg_pid_.clear();
+    right_deg_pid_.clear();
+    left_length_pid_.clear();
+    right_length_pid_.clear();
+    left_omega_pid_.clear();
+    right_omega_pid_.clear();
+}
---
+++ APP/chassis/controller/app_leg_ctrl.h
@@ -0,0 +1,49 @@
+//
+// Created by 15082 on 2026/1/24.
+//
+
+#ifndef APP_LEG_CTRL_H
+#define APP_LEG_CTRL_H
+#include "app_relay.h"
+#include "ctrl_pid.h"
+
+namespace Controller {
+    typedef struct {
+        float32_t tor_left, tor_right;
+        float32_t force_left, force_right;
+    }leg_output;
+    class app_leg_ctrl {
+    public:
+        app_leg_ctrl() = default;
+        app_leg_ctrl(const PID::pid_param_t& speed_param,
+                     const PID::pid_param_t& len_param,
+                     const PID::pid_param_t& omega_param,
+                     const PID::pid_param_t& deg_param)
+            : left_speed_pid_(speed_param), right_speed_pid_(speed_param),
+            left_length_pid_(len_param), right_length_pid_(len_param),
+            left_omega_pid_(omega_param), right_omega_pid_(omega_param),
+            left_deg_pid_(deg_param), right_deg_pid_(deg_param) {
+        }
+        void left_len_update(Relay::leg_status left_leg, float target_len);
+        void right_len_update(Relay::leg_status right_leg, float target_len);
+        void left_deg_update(Relay::leg_status left_leg, float target_deg);
+        void right_deg_update(Relay::leg_status right_leg, float target_deg);
+        leg_output get_output() {return output_;}
+        void leg_clear();
+    private:
+        PID left_speed_pid_;
+        PID right_speed_pid_;
+        PID left_length_pid_;
+        PID right_length_pid_;
+        PID left_omega_pid_;
+        PID right_omega_pid_;
+        PID left_deg_pid_;
+        PID right_deg_pid_;
+        leg_output output_ = {0};
+    };
+}
+
+
+
+
+#endif //APP_LEG_CTRL_H
